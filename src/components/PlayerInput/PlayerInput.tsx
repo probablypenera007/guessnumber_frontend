fde0b5a6a6baf32ad13b14b5065c8baa84fccaff
James Penera <jamespenera@icloud.com>
2024-05-01 03:34:33 +0400

    playerinput styling

--- src/components/PlayerInput/PlayerInput.tsx
+++ src/components/PlayerInput/PlayerInput.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {TiArrowSortedDown, TiArrowSortedUp} from "react-icons/ti";
+import { TiArrowSortedDown, TiArrowSortedUp } from "react-icons/ti";
 
 //Internal imports
 import Style from "./PlayerInput.module.css";
@@ -10,26 +10,29 @@ const PlayerInput = () => {
       <div className={Style.playerInput__container}>
         <div className={Style.playerInput__points}>
           <button className={Style.playerInput__points_down}>
-            <TiArrowSortedDown />
+            <TiArrowSortedDown className={Style.icon} />
           </button>
           <p className={Style.playerInput__point_text}>Points</p>
-          <input type="number" className={Style.playerInput__points_input} />
+          <input type="number" placeholder="50" className={Style.playerInput__points_input} disabled/>
           <button className={Style.playerInput__points_up}>
-            <TiArrowSortedUp />
+            <TiArrowSortedUp className={Style.icon} />
           </button>
         </div>
+
         <div className={Style.playerInput__multiplier}>
           <button className={Style.playerInput__multiplier_down}>
-            <TiArrowSortedDown/>
+            <TiArrowSortedDown className={Style.icon} />
           </button>
           <p className={Style.playerInput__multiplier_text}>Multiplier</p>
           <input
             type="number"
+            placeholder="0.5"
             className={Style.playerInput__multiplier_input}
+            disabled
           />
 
           <button className={Style.playerInput__multipilier_up}>
-            <TiArrowSortedUp/>
+            <TiArrowSortedUp className={Style.icon} />
           </button>
         </div>
       </div>
