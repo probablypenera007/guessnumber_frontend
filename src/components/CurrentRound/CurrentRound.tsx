a13411cdcab1380cbda33fe63b49a0eab8f0e8df
James Penera <jamespenera@icloud.com>
2024-05-01 21:52:01 +0400

    current round

--- src/components/CurrentRound/CurrentRound.tsx
+++ src/components/CurrentRound/CurrentRound.tsx
@@ -15,28 +15,39 @@ const CurrentRound: React.FC<CurrentRoundProps> = ({
   const defaultPlayers = ["You", "CPU 1", "CPU 2", "CPU 3", "CPU 4"];
 
   const renderTableRows = () => {
-    if (!gameStarted) {
-      return defaultPlayers.map((player) => (
-        <tr key={player}>
-          <td className={Style.table__data}>{player}</td>
-          <td className={Style.table__data}>-</td>
-          <td className={Style.table__data}>-</td>
-        </tr>
-      ));
-    }
+    const playerData = gameStarted ? players : defaultPlayers.map(player => ({ name: player, points: '-', multiplier: '-' }));
 
-    return players.map((player) => (
-      <tr key={player.name}>
+    return playerData.map((player, index) => {
+      let rowStyle: string = '';
+      switch (index) {
+        case 0:
+          rowStyle = Style["table__row--you"] as string;
+          break;
+        case 1:
+        case 3:
+          rowStyle = Style["table__row--cpu1"] as string;
+          break;
+        case 2:
+        case 4:
+          rowStyle = Style["table__row--cpu2"] as string;
+          break;
+        default:
+          break;
+      }
+
+    return (
+      <tr key={player.name} className={rowStyle}>
         <td className={Style.table__data}>{player.name}</td>
-        <td className={Style.table__data}>{player.points}</td>
-        <td className={Style.table__data}>{player.multiplier}</td>
+        <td className={Style.table__data}>{typeof player.points === 'number' ? player.points : '-'}</td>
+        <td className={Style.table__data}>{typeof player.multiplier === 'number' ? player.multiplier : '-'}</td>
       </tr>
-    ));
-  };
+    );
+  });
+};
 
   return (
     <div className={Style.currentRound}>
-      <h3 className={Style.currentRound__title}>Current Round</h3>
+      <h3 className={Style.currentRound__title}>icon Current Round</h3>
       <table className={Style.currentRound__table}>
         <thead className={Style.currentRound__tableHeader}>
           <tr className={Style.table__row}>
