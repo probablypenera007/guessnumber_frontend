ff585ea54c4d84e0892bf4038bbebad2c408001a
James Penera <jamespenera@icloud.com>
2024-05-01 16:20:29 +0400

    Update cpuPlayers.ts

--- src/utils/cpuPlayers.ts
+++ src/utils/cpuPlayers.ts
@@ -1,1 +1,13 @@
+interface CPUPlayer {
+    name: string;
+    points: number;
+    multiplier: number;
+}
+
+export const generateInitialCPUPlayers = (): CPUPlayer[] => [
+    { name: "CPU 1", points: 0, multiplier: 0 },
+    { name: "CPU 2", points: 0, multiplier: 0 },
+    { name: "CPU 3", points: 0, multiplier: 0 },
+    { name: "CPU 4", points: 0, multiplier: 0 }
+]
 
