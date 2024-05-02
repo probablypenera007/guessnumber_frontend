e6c04df9b67a7f8010187ae47c6f08d521869ff4
James Penera <jamespenera@icloud.com>
2024-05-02 11:33:09 +0400

    Update Ranking.tsx

---
+++ src/components/Ranking/Ranking.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+//internal imports
+import Style from './Ranking.module.scss'
+
+
+const Ranking = () => {
+  return (
+    <div>Ranking</div>
+  )
+}
+
+export default Ranking
