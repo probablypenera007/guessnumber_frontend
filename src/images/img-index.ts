d8cc5a7fcf6273b2d4081e792b45745460a6657b
James Penera <jamespenera@icloud.com>
2024-05-02 16:43:28 +0400

    Create img-index.ts

---
+++ src/images/img-index.ts
@@ -0,0 +1,19 @@
+import chatLogo from './chat-logo.png';
+import currentRoundLogo from './current-round-logo.png';
+import pointsLogo from './points-logo.png';
+import rankingLogo from './ranking-logo.png';
+import speedLogo from './speed-logo.png';
+import timerLogo from './timer-logo.png';
+import userActiveLogo from './user-active.png';
+import userInactiveLogo from './user-inactive.png';
+
+export default {
+    chatLogo,
+    currentRoundLogo,
+    pointsLogo,
+    rankingLogo,
+    speedLogo,
+    timerLogo,
+    userActiveLogo,
+    userInactiveLogo,
+}
