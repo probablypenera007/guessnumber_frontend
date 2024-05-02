638e8cf5b9c1157bedce46cb1b8ee03added2a45
James Penera <jamespenera@icloud.com>
2024-05-02 11:35:01 +0400

    chatbox created

---
+++ src/components/ChatBox/ChatBox.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+//internal imports
+import Style from './ChatBox.module.scss'
+
+
+const ChatBox = () => {
+  return (
+    <div>ChatBox</div>
+  )
+}
+
+export default ChatBox
