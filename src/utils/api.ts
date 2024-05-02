eb9f510363ecf3291e350940bebdd393ac36e476
James Penera <jamespenera@icloud.com>
2024-05-03 01:24:39 +0400

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,4 @@
-
 export const baseUrl = "http://localhost:3001";
-
 
 export function checkResponse(res: Response): Promise<any> {
   if (res.ok) {
@@ -11,34 +9,35 @@ export function checkResponse(res: Response): Promise<any> {
 }
 
 export function request(url: string, options: RequestInit): Promise<any> {
-  return fetch(url, options).then(checkResponse);
+  const defaultOptions: RequestInit = {
+    ...options,
+    headers: {
+      ...options.headers,
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include' as RequestCredentials, 
+  };
+  return fetch(url, defaultOptions).then(checkResponse);
 }
-
 
 export function getPlayers(url: string): Promise<any> {
     return request(`${baseUrl}${url}`, {
       method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
     });
-  }
+}
 
-  interface RegisterData {
-    name: string;
-  }
-  
-  export const register = (userData: RegisterData): Promise<any> => {
-    const dataToSend = {
-      ...userData,
-      points: 1000 
-    };
-  
-    return request(`${baseUrl}/players`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(dataToSend),
-    });
+interface RegisterData {
+  name: string;
+}
+
+export const register = (userData: RegisterData): Promise<any> => {
+  const dataToSend = {
+    ...userData,
+    points: 1000
   };
+
+  return request(`${baseUrl}/players`, {
+    method: "POST",
+    body: JSON.stringify(dataToSend),
+  });
+};
